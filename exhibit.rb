c9a2b8560c61dd6553dacd0bf445bc99c8be8bd5
Sam Craig <craigsamm@gmail.com>
2020-04-13 15:27:30 -0400

    Example of loading config

--- exhibit.rb
+++ exhibit.rb
@@ -1,6 +1,12 @@
 require "sinatra"
+
+set :environment, :development
 require "sinatra/reloader" if development?
 
+config = JSON.parse(File.read('config.json'))
+
+puts config
+
 get "/" do
-  "Hello world!"
+  config.to_json
 end
