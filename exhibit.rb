4ab960869aa39670e660cd0d906ce376dd99703f
Sam Craig <craigsamm@gmail.com>
2020-04-13 23:18:16 -0400

    Error handling for missing configuration

--- exhibit.rb
+++ exhibit.rb
@@ -2,6 +2,10 @@ require "sinatra"
 require "sinatra/reloader" if development?
 
 get "/:exhibit" do
+  button_path = "config/#{params[:exhibit]}.json"
+
+  halt 404, "No configuration exists for that presentation" unless File.file? button_path
+
   @buttons = JSON.parse(File.read("config/#{params[:exhibit]}.json"), object_class: OpenStruct)
   erb :exhibit
 end
