c5087a9364bbef19209fdaac533f243aa5fe332a
Oliver Ripps <oliver.ripps@gmail.com>
2020-04-16 20:18:15 -0400

    new svgs

--- exhibit.rb
+++ exhibit.rb
@@ -7,6 +7,8 @@ get "/:exhibit" do
   halt 404, "No configuration exists for that presentation" unless File.file? button_path
 
   @buttons = JSON.parse(File.read("config/#{params[:exhibit]}.json"), object_class: OpenStruct)
+  @initial = @buttons.find{|e| e.initial}
   puts @buttons
+  puts @initial
   erb :exhibit
 end
