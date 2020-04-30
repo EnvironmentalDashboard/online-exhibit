4974bbb64c85ec1ff7d80ae6fc601170e7ab9410
Oliver Ripps <oliver.ripps@gmail.com>
2020-04-30 18:57:10 -0400

    code review changes

--- exhibit.rb
+++ exhibit.rb
@@ -1,35 +1,45 @@
 require "sinatra"
 require "sinatra/reloader" if development?
 
+
+def button_index (buttonslist, index)
+  listlength = buttonslist.length
+  index = index.to_i
+  index = (index >= listlength ? 0: index)
+  return index
+end
+
+def setup()
+  buttonslist = JSON.parse(File.read("config/#{params[:exhibit]}.json"), object_class: OpenStruct)
+  vars={}
+
+
+
 get "/:exhibit" do
   button_path = "config/#{params[:exhibit]}.json"
 
   halt 404, "No configuration exists for that presentation" unless File.file? button_path
 
-  @buttonslist = JSON.parse(File.read("config/#{params[:exhibit]}.json"), object_class: OpenStruct)
-  @buttons = @buttonslist[0]
+  buttonslist = JSON.parse(File.read("config/#{params[:exhibit]}.json"), object_class: OpenStruct)
+  @buttons = buttonslist[0]
   @initial = @buttons.find{|e| e.initial}
   @button_index = 0
   @exhibit = params[:exhibit]
   @buttons_html = erb :controller
-  puts @buttons
-  puts @initial
   erb :exhibit
 end
 
 get "/:exhibit/buttons" do
 
-  button_path = "config/#{params[:exhibit]}.json"
+  button_set_path = "config/#{params[:exhibit]}.json"
 
-  halt 404, "No configuration exists for that presentation" unless File.file? button_path
+  halt 404, "No configuration exists for that presentation" unless File.file? button_set_path
 
   @exhibit = params[:exhibit]
-  @buttonslist = JSON.parse(File.read("config/#{params[:exhibit]}.json"), object_class: OpenStruct)
-  @button_index = params[:index] || 0
-  @listlength = @buttonslist.length
-  @button_index = @button_index.to_i
-  @button_index = (@button_index>=@listlength? 0: @button_index)
-  @buttons = @buttonslist[@button_index]
+  buttonslist = JSON.parse(File.read("config/#{params[:exhibit]}.json"), object_class: OpenStruct)
+  index = params[:index] || 0
+  @button_index= button_index(buttonslist, index)
+  @buttons=buttonslist[@button_index]
   @initial = @buttons.find{|e| e.initial}
 
   erb :controller
@@ -38,16 +48,14 @@ end
 
 get "/:exhibit/initial" do
 
-  button_path = "config/#{params[:exhibit]}.json"
+  button_set_path = "config/#{params[:exhibit]}.json"
 
-  halt 404, "No configuration exists for that presentation" unless File.file? button_path
+  halt 404, "No configuration exists for that presentation" unless File.file? button_set_path
 
-  @buttonslist = JSON.parse(File.read("config/#{params[:exhibit]}.json"), object_class: OpenStruct)
-  @button_index = params[:index] || 0
-  @listlength = @buttonslist.length
-  @button_index = @button_index.to_i
-  @button_index = (@button_index>=@listlength? 0: @button_index)
-  @buttons = @buttonslist[@button_index]
+  buttonslist = JSON.parse(File.read("config/#{params[:exhibit]}.json"), object_class: OpenStruct)
+  index = params[:index] || 0
+  @button_index= button_index(buttonslist, index)
+  @buttons=buttonslist[@button_index]
   @initial = @buttons.find{|e| e.initial}
 
   @initial.pres_path
